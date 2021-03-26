6c09e968556500db2cc6877b2d1ac2bcfbe992ce
Gustavo Landfried <gustavolandfried@gmail.com>
2021-03-26 12:35:37 -0300

    Example: skill evolution

--- examples/examples.R
+++ examples/examples.R
@@ -1,13 +1,12 @@
 source("../TrueSkill.R")
 
 # Code 1
-mu = 0.0; sigma = 6.0; beta = 1.0; gamma = 0.0
-p1 = Player(Gaussian(mu, sigma), beta, gamma); p2 = Player(Gaussian(mu, sigma), beta, gamma);
-p3 = Player(Gaussian(mu, sigma), beta, gamma); p4 = Player(Gaussian(mu, sigma), beta, gamma);
+mu = 0.0; sigma = 6.0; beta = 1.0; gamma = 0.0; p_draw = 0.0
+a1 = Player(Gaussian(mu, sigma), beta, gamma); a2 = Player(); a3 = Player(); a4 = Player()
 
 # Code 2
-team_a = c(p1, p2)
-team_b = c(p3, p4)
+team_a = c(a1, a2)
+team_b = c(a3, a4)
 teams = list(team_a, team_b)
 g = Game(teams)
 # g = Game(teams, c(0,0))
@@ -24,9 +23,9 @@ print(pos[[1]][1])
 print(lhs[[1]][[1]] * p1$prior)
 
 # Code 5
-ta = c(p1)
-tb = c(p2, p3)
-tc = c(p4)
+ta = c(a1)
+tb = c(a2, a3)
+tc = c(a4)
 teams = list(ta, tb, tc)
 result = c(1, 0, 0)
 g = Game(teams, result, p_draw=0.25)
@@ -40,16 +39,37 @@ h = History(composition, gamma=0.0)
 
 # Code 7
 lc = h$learning_curves()
-lc.print(lc$a)
-lc.print(lc$b)
+lc_print(lc$a)
+lc_print(lc$b)
 
 # Code 8
 h$convergence()
 lc = h$learning_curves()
-lc.print(lc$a)
-lc.print(lc$b)
+lc_print(lc$a)
+lc_print(lc$b)
 
-#
+
+# Skill evolution
+
+N = 1000
+skill <- function(experience, middle, maximum, slope){
+    return(maximum/(1+exp(slope*(-experience+middle))))
+}
+target = skill(seq(N), 500, 2, 0.0075)
+opponents = rnorm(N,target,0.5)
+
+composition = list(); results = list(); times = c(); priors = hash()
+for(i in seq(N)){composition[[i]] = list(c("a"), c(toString(i)))}
+for(i in seq(N)){results[[i]] = if(rnorm(1,target[i])>rnorm(1,opponents[i])){c(1,0)}else{c(0,1)} }
+for(i in seq(N)){times = c(times,i)}
+for(i in seq(N)){priors[[toString(i)]] = Player(Gaussian(opponents[i],0.2))}
+    
+h = History(composition, results, times, priors, gamma=0.015)
+h$convergence()
+lc_a = h$learning_curves()$a; mu = c()
+for(tp in lc_a){mu = c(mu,tp[[2]]$mu)}
+
+##############
 
 data = read.csv("input/history.csv", header=T)
 get.composition = function(x){
@@ -64,9 +84,3 @@ get.composition = function(x){
     return(res)
 }
 
-names(data)
-data$time_start[1]
-
-composition = apply(data, MARGIN=1, FUN=get.composition)
-
-composition[[1]] 

a595a5e8f61029fde5c59877a8d587535859c610
Gustavo Landfried <gustavolandfried@gmail.com>
2021-04-05 10:58:34 -0300

    src: Epsilon and iterations. Examples: atp

---
+++ examples/atp.R
@@ -0,0 +1,29 @@
+source("../TrueSkill.R")
+
+data = read.csv("input/history.csv", header=T)
+
+get_composition = function(x){
+    res = list()
+    if (x["double"]=="t"){
+        res[[1]] = c(x["w1_name"],x["w2_name"])
+        res[[2]] = c(x["l1_name"],x["l2_name"])
+    }else{
+        res[[1]] = c(x["w1_name"])
+        res[[2]] = c(x["l1_name"])
+    }
+    return(res)
+}
+
+composition =  apply(data, 1, get_composition ) 
+times = as.Date(data[,"time_start"], format = "%Y-%m-%d")
+times = times - as.Date("1910-01-01", format = "%Y-%m-%d")
+times = as.numeric(times )
+
+start.time <- Sys.time()
+h = History(composition = composition, times = times, sigma = 1.6, gamma = 0.036)
+h$convergence(epsilon=0.01, iterations=10)
+end.time <- Sys.time()
+print(end.time - start.time)
+
+
+

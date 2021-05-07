0626336911baa84d347d57b1b0918e8f4fb3e0c7
Gustavo Landfried <gustavolandfried@gmail.com>
2021-05-07 12:55:12 -0300

    Examples

--- examples/examples.R
+++ examples/examples.R
@@ -3,44 +3,44 @@ library(microbenchmark)
 library(profvis)
 
 
-# Code 1
+print("Code 1")
 mu = 0.0; sigma = 6.0; beta = 1.0; gamma = 0.03; p_draw = 0.0
+
+print("Code 2")
 a1 = Player(Gaussian(mu, sigma), beta, gamma); a2 = Player(); a3 = Player(); a4 = Player()
 
-1- cdf(0,2,sqrt(2))
-
-# Code 2
+print("Code 3")
 team_a = c(a1, a2)
 team_b = c(a3, a4)
 teams = list(team_a, team_b)
 g = Game(teams)
 # g = Game(teams, c(0,0))
-microbenchmark(posteriors(Game(teams)), times=100, unit="s")
+microbenchmark(posteriors(Game(teams)), times=10, unit="s")
 #profvis(microbenchmark(posteriors(Game(teams)), times=100, unit="s"))
 
-# Code 3
+print("Code 4")
 lhs = g@likelihoods
 ev = g@evidence
 ev = round(ev, 3)
 
-# Code 4
+print("Code 5")
 pos = posteriors(g)
 print(pos[[1]][1])
 print(lhs[[1]][[1]] * a1@prior)
 
-# Code 5
+print("Code 6")
 ta = c(a1)
 tb = c(a2, a3)
 tc = c(a4)
 teams = list(ta, tb, tc)
 result = c(1, 0, 0)
 g = Game(teams, result, p_draw=0.25)
-microbenchmark(posteriors(Game(teams, result, p_draw=0.25)), times=20, unit="s")
+microbenchmark(posteriors(Game(teams, result, p_draw=0.25)), times=10, unit="s")
 
 #profvis(Game(teams))
-profvis(microbenchmark(posteriors(Game(teams, result, p_draw=0.25)), times=20, unit="s"))
+#profvis(microbenchmark(posteriors(Game(teams, result, p_draw=0.25)), times=20, unit="s"))
 
-# Code 6
+print("Code 7")
 c1 = list(c("a"),c("b"))
 c2 = list(c("b"),c("c"))
 c3 = list(c("c"),c("a"))
@@ -48,12 +48,12 @@ composition = list(c1,c2,c3)
 h = History(composition, gamma=0.0)
 microbenchmark(History(composition, gamma=0.0), times=20, unit="s")
 
-# Code 7
+print("Code 8")
 lc = h$learning_curves()
 lc_print(lc$a)
-# lc_print(lc$b)
+lc_print(lc$b)
 
-# Code 8
+print("Code 9")
 h$convergence()
 microbenchmark(h$convergence(iterations=1,verbose=F), times=10, unit="s")
 lc = h$learning_curves()
@@ -61,7 +61,7 @@ lc_print(lc$a)
 lc_print(lc$b)
 
 # Skill evolution
-
+print("Code 10")
 N = 1000
 skill <- function(experience, middle, maximum, slope){
     return(maximum/(1+exp(slope*(-experience+middle))))
@@ -69,6 +69,7 @@ skill <- function(experience, middle, maximum, slope){
 target = skill(seq(N), 500, 2, 0.0075)
 opponents = rnorm(N,target,0.5)
 
+print("Code 11")
 composition = list(); results = list(); times = c(); priors = hash()
 for(i in seq(N)){composition[[i]] = list(c("a"), c(toString(i)))}
 for(i in seq(N)){results[[i]] = if(rnorm(1,target[i])>rnorm(1,opponents[i])){c(1,0)}else{c(0,1)} }
@@ -76,48 +77,7 @@ for(i in seq(N)){times = c(times,i)}
 for(i in seq(N)){priors[[toString(i)]] = Player(Gaussian(opponents[i],0.2))}
 
 h = History(composition, results, times, priors, gamma=0.015)
-microbenchmark(History(composition, results, times, priors, gamma=0.015), times=1, unit="s")
-microbenchmark(h$convergence(iterations=1), times=1, unit="s")
+#microbenchmark(History(composition, results, times, priors, gamma=0.015), times=1, unit="s")
+#microbenchmark(h$convergence(iterations=1), times=1, unit="s")
 lc_a = h$learning_curves()$a; mu = c()
 for(tp in lc_a){mu = c(mu,tp[[2]]@mu)}
-
-#
-
-data = read.csv("input/smoothing.csv", stringsAsFactors=FALSE)
-colnames(data)
-
-p_d_m = 0 # p(D|M) & d1: prior & d2: prior*likelihood_1^1 
-p_d_m_approx = 0 # \widehat{p}(D|M) & d1: prior & d2: prior*likelihood_1^k 
-loocv = 0 # LOOCV & d1: prior*likelihood_2^k & d2: prior*likelihood_1^k
-
-sigma0 <- 3.0
-mu0 <- 0.0
-beta <- 0.5
-
-prior = Gaussian(0.0,3.0)
-# p(D|M) & d1: prior & d2: prior*likelihood_1^1 
-lha_1_1 = Gaussian(data$lh_a_1_mu[1],data$lh_a_1_sigma[1])
-lhb_1_1 = Gaussian(data$lh_b_1_mu[1],data$lh_b_1_sigma[1])
-a2 = Player(prior=prior*lha_1_1, beta=beta)
-b2 = Player(prior=prior*lhb_1_1, beta=beta)
-e2 = Game(list(c(a2),c(b2)),c(0,1))@evidence
-p_d_m = 0.5 * e2
-
-# \widehat{p}(D|M) & d1: prior & d2: prior*likelihood_1^k 
-k = length(data$lh_a_1_mu)
-lha_1_k = Gaussian(data$lh_a_1_mu[k],data$lh_a_1_sigma[k])
-lhb_1_k = Gaussian(data$lh_b_1_mu[k],data$lh_b_1_sigma[k])
-a2 = Player(prior=prior*lha_1_k, beta=beta)
-b2 = Player(prior=prior*lhb_1_k, beta=beta)
-e2 = Game(list(c(a2),c(b2)),c(0,1))@evidence
-p_d_m_approx = 0.5 * e2
-
-# LOOCV & d1: prior*likelihood_2^k & d2: prior*likelihood_1^k
-lha_2_k = Gaussian(data$lh_a_2_mu[k],data$lh_a_2_sigma[k])
-lhb_2_k = Gaussian(data$lh_b_2_mu[k],data$lh_b_2_sigma[k])
-a2 = Player(prior=prior*lha_2_k, beta=beta)
-b2 = Player(prior=prior*lhb_2_k, beta=beta)
-e1 = Game(list(c(a2),c(b2)),c(1,0))@evidence
-
-p_d_m_approx = 0.5 
-
